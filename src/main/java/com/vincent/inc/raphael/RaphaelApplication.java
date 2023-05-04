fa8696746c336d41ecf4a2634e754740ea73d9ce
VincentB00 <vincentbui1999@gmail.com>
2023-05-04 10:42:46 -0400

    fix redirect to swagger

--- src/main/java/com/vincent/inc/raphael/RaphaelApplication.java
+++ src/main/java/com/vincent/inc/raphael/RaphaelApplication.java
@@ -4,8 +4,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @SpringBootApplication
@@ -20,13 +22,13 @@ public class RaphaelApplication {
 		SpringApplication.run(RaphaelApplication.class, args);
 	}
 
-	@GetMapping()
-	public String healthCheck1() {
-		return String.format("Raphael %s is up and running", env);
+	@GetMapping("/")
+	public ModelAndView redirectSwagger(ModelMap model) {
+		return new ModelAndView("redirect:/swagger-ui/index.html", model);
 	}
 
 	@GetMapping("/_status/healthz")
-	public String healthCheck2() {
+	public String healthCheck() {
 		return String.format("Raphael %s is up and running", env);
 	}
 

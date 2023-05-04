a8d570e1cbdc6429df9b7c2c6273c5156cd50a69
VincentB00 <vincentbui1999@gmail.com>
2023-05-04 10:04:37 -0400

    init

---
+++ src/main/java/com/vincent/inc/raphael/RaphaelApplication.java
@@ -0,0 +1,13 @@
+package com.vincent.inc.raphael;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaphaelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaphaelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vincent/inc/raphael/RaphaelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincent.inc.raphael;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaphaelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

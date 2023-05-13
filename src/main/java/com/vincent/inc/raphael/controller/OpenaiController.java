1459a0cc9409039975ed3a809571eca6eb3aba66
VincentB00 <vincentbui1999@gmail.com>
2023-05-13 01:58:53 -0400

    add chat for openai service

--- src/main/java/com/vincent/inc/raphael/controller/OpenaiController.java
+++ src/main/java/com/vincent/inc/raphael/controller/OpenaiController.java
@@ -3,13 +3,13 @@ package com.vincent.inc.raphael.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.theokanning.openai.completion.CompletionChoice;
+import com.theokanning.openai.completion.chat.ChatCompletionChoice;
 import com.vincent.inc.raphael.model.Promt;
 import com.vincent.inc.raphael.service.OpenaiService;
 
@@ -20,8 +20,13 @@ public class OpenaiController {
     @Autowired
     private OpenaiService openaiService;
     
-    @PostMapping
+    @PostMapping("promt")
     public List<CompletionChoice> promt(@RequestBody Promt promt) {
         return this.openaiService.promt(promt);
+    }
+    
+    @PostMapping("chat")
+    public List<ChatCompletionChoice> chat(@RequestBody Promt promt) {
+        return this.openaiService.chat(promt);
     }
 }
--- src/main/java/com/vincent/inc/raphael/service/OpenaiService.java
+++ src/main/java/com/vincent/inc/raphael/service/OpenaiService.java
@@ -1,5 +1,6 @@
 package com.vincent.inc.raphael.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -7,6 +8,9 @@ import org.springframework.stereotype.Service;
 
 import com.theokanning.openai.completion.CompletionChoice;
 import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.completion.chat.ChatCompletionChoice;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
 import com.theokanning.openai.service.OpenAiService;
 import com.vincent.inc.raphael.model.Promt;
 
@@ -15,17 +19,39 @@ public class OpenaiService {
 
     OpenAiService ai;
 
+    List<ChatMessage> chatList;
+
     public OpenaiService(@Value("${openai.token}") String token) {
         ai = new OpenAiService(token);
+        this.chatList = new ArrayList<>();
+        chatList.add(new ChatMessage("system", "A sassy and tired personal assistant"));
     }
+
+    
 
     public List<CompletionChoice> promt(Promt promt) {
 
         CompletionRequest completionRequest = CompletionRequest.builder()
         .prompt(promt.getMessage())
-        .model("gpt-3.5-turbo")
+        .model("text-davinci-003")
         .echo(true)
         .build();
         return ai.createCompletion(completionRequest).getChoices();
+    }
+
+    public List<ChatCompletionChoice> chat(Promt promt) {
+        this.chatList.add(new ChatMessage("user", promt.getMessage()));
+        ChatCompletionRequest chatCompletionRequest = ChatCompletionRequest
+        .builder()
+        .model("gpt-3.5-turbo")
+        .temperature(0.8)
+        .messages(this.chatList)
+        .build();
+
+        var response = ai.createChatCompletion(chatCompletionRequest).getChoices();
+
+        this.chatList.add(new ChatMessage("assistant", response.get(0).getMessage().getContent()));
+        
+        return ai.createChatCompletion(chatCompletionRequest).getChoices();
     }
 }

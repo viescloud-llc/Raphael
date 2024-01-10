2a440b69aaa7a869bdfb1e4c0c761770003df909
VincentB00 <vincentbui1999@gmail.com>
2024-01-09 21:02:14 -0500

    update vk config

--- src/test/java/com/vincent/inc/raphael/RaphaelApplicationTests.java
+++ src/test/java/com/vincent/inc/raphael/RaphaelApplicationTests.java
@@ -1,13 +1,5 @@
 package com.vincent.inc.raphael;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class RaphaelApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }

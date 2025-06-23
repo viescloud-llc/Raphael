600dbf4c6500fa3e5c8fcd7e34d409d981ee47c9
VincentB00 <vincentbui1999@gmail.com>
2025-06-22 23:33:44 -0400

    feat: Update vies-spring-utils dependency to version 5.7.5

--- src/main/java/com/vincent/inc/raphael/RaphaelApplication.java
+++ src/main/java/com/vincent/inc/raphael/RaphaelApplication.java
@@ -6,14 +6,12 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.viescloud.eco.viesspringutils.auto.config.ViesBeanConfig;
 
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
-@RestController
 @Import(ViesBeanConfig.class)
 @EnableJpaRepositories
 @EntityScan
